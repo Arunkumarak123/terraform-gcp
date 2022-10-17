45734e76a0cb2a0befd26304dfe0507d96dc4367
arunkumar <102405678+Arunkumarak123@users.noreply.github.com>
2022-10-17 17:05:28 +0530

    Update firewall.tf

--- firewall.tf
+++ firewall.tf
@@ -1,13 +1,13 @@
 resource "google_compute_firewall" "rules" {
-  name        = "my-firewall-rule"
-  network     = "terraform-vpc"
+  name        = "dev-firewall-rule"
+  network     = "dev-vpc"
   description = "Creates firewall rule targeting tagged instances"
 
   allow {
     protocol  = "tcp"
     ports     = ["80", "8080", "1000-2000","22"]
   }
-  source_tags = ["dev-public-subnet1"]
+  source_tags = ["dev-public-subnet-1"]
   target_tags = ["dev-private-subnet-1"]
 
 }
