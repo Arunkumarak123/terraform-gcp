675572ef139532dda78f30086b08957ecfc5d5a6
arunkumar <102405678+Arunkumarak123@users.noreply.github.com>
2022-10-17 16:20:28 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -13,6 +13,16 @@ name = "subnet1"
 ip_cidr_range = "10.2.0.0/16"
 region = "asia-south1"
 network = google_compute_network.vpc_network.name
+secondary_ip_range {
+    range_name    = "sec-range-1"
+    ip_cidr_range = "10.0.26.0/24"
+  }
+secondary_ip_range {
+    range_name    = "sec-range-2"
+    ip_cidr_range = "10.0.28.0/24"
+  }
+
+
 }
 resource "google_compute_subnetwork" "private-subnetwork" {
 name = "subnet2"
@@ -20,4 +30,8 @@ private_ip_google_access = "true"
 ip_cidr_range = "10.3.0.0/16"
 region = "asia-south1"
 network = google_compute_network.vpc_network.name
+secondary_ip_range {
+    range_name    = "sec-range-3"
+    ip_cidr_range = "10.0.22.0/24"
+  }
 }
