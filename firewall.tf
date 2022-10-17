758fcf88c102f61db5f533969ee32171b34e6b0d
arunkumar <102405678+Arunkumarak123@users.noreply.github.com>
2022-10-17 16:34:46 +0530

    Create firewall.tf

---
+++ firewall.tf
@@ -0,0 +1,13 @@
+resource "google_compute_firewall" "rules" {
+  name        = "my-firewall-rule"
+  network     = "terraform-vpc"
+  description = "Creates firewall rule targeting tagged instances"
+
+  allow {
+    protocol  = "tcp"
+    ports     = ["80", "8080", "1000-2000","22"]
+  }
+  source_tags = ["subnet1"]
+  target_tags = ["subnet2"]
+
+}
