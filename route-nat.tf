9fd45984cf9ee88249e1048a70674c4e79a5b7a5
arunkumar <102405678+Arunkumarak123@users.noreply.github.com>
2022-10-17 17:04:57 +0530

    Update route-nat.tf

--- route-nat.tf
+++ route-nat.tf
@@ -1,10 +1,10 @@
-resource "google_compute_router_nat" "terraform-vpc" {
+resource "google_compute_router_nat" "dev-vpc" {
   name                               = "dev-nat"
   router                             = "dev-router"
   nat_ip_allocate_option             = "MANUAL_ONLY"
   source_subnetwork_ip_ranges_to_nat = "LIST_OF_SUBNETWORKS"
   subnetwork {
-    name = "dev-public-subnet1"
+    name = "dev-public-subnet-1"
     source_ip_ranges_to_nat = ["PRIMARY_IP_RANGE"]
   }
 }
